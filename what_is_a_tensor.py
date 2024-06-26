957381bfa8ce4d02dcb6555ae60e739bac824872
erenhatipoglu <134336208+erenhatipoglu@users.noreply.github.com>
2024-06-26 21:49:56 +0300

    Create what_is_a_tensor.py

---
+++ what_is_a_tensor.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+
+a = np.array(1) # scalar
+
+b = np.array([2,2,2]) # vector
+
+c = np.array([[1,1,1],
+              [2,2,2],
+              [3,3,3]]) # matrix
+
+d = np.array([[[1,1,1],[2,2,2]],
+              [[3,3,3],[4,4,4]]]) # tensor
